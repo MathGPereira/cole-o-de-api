79ec5f32054d1ce015050e07918e38fcb09e972d
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-12 12:25:07 -0300

    Criando novas pastas para melhorar a organização dos códigos

---
+++ assets/js/api/apod.js
@@ -0,0 +1,9 @@
+// async function api(data="", hd="False", count="", start_date="", end_date="", thumbs="False") {
+//     const url = `https://api.nasa.gov/planetary/apod?api_key=D11v7QjGEbaTNyv33dKUWZVE2Dt6qwU9jFca9Uhg&${data}&${resolucao}&${quantidadeImagens}&${dataInicio}&${dataFinal}&${thumbImagem}&`;
+//     const apod = await fetch(url);
+//     const apodJSON = await apod.json();
+
+//     console.log(apodJSON);
+// }
+
+// api(dataImagem, resolucao, quantidadeImagens, dataInicio, dataFinal, thumbImagem);
