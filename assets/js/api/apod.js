9ee8a4047ec567773bf6d162ee8f0784fce012ae
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-12 13:46:02 -0300

    Consumo da API APOD pronta e primeira seção pronta

--- assets/js/api/apod.js
+++ assets/js/api/apod.js
@@ -1,9 +1,35 @@
-// async function api(data="", hd="False", count="", start_date="", end_date="", thumbs="False") {
-//     const url = `https://api.nasa.gov/planetary/apod?api_key=D11v7QjGEbaTNyv33dKUWZVE2Dt6qwU9jFca9Uhg&${data}&${resolucao}&${quantidadeImagens}&${dataInicio}&${dataFinal}&${thumbImagem}&`;
-//     const apod = await fetch(url);
-//     const apodJSON = await apod.json();
+async function api(data="", hd="false", count="", start_date="", end_date="", thumbs="false") {
+    const url = `https://api.nasa.gov/planetary/apod?api_key=D11v7QjGEbaTNyv33dKUWZVE2Dt6qwU9jFca9Uhg&count=${count}`;
+    const apod = await fetch(url);
+    const apodJSON = await apod.json();
 
-//     console.log(apodJSON);
-// }
+    colocaImagemNaPagina(apodJSON);
+}
 
-// api(dataImagem, resolucao, quantidadeImagens, dataInicio, dataFinal, thumbImagem);
+function colocaImagemNaPagina(urls) {
+    const secao = document.querySelector("[data-sobre-api]");
+
+    if(urls.length === 1) {
+        const imagem = `
+            <img src="${urls[0].url}" alt="Exemplo de uma imagem astronômica dada pela API" class="sobre-api__imagem-exemplo" data-imagem-exemplo>
+        `;
+        
+        secao.innerHTML += imagem;
+    }else if(urls.length > 1) {
+        urls.forEach(url => {
+            const imagem = `
+                <img src="${url.url}" alt="Exemplo de uma imagem astronômica dada pela API" class="sobre-api__imagem-exemplo" data-imagem-exemplo>
+            `;
+
+            secao.innerHTML += imagem;
+        });
+    }else {
+        const imagem = `
+            <img src="${urls.url}" alt="Exemplo de uma imagem astronômica dada pela API" class="sobre-api__imagem-exemplo" data-imagem-exemplo>
+        `;
+        
+        secao.innerHTML += imagem;
+    }
+}
+
+api();
