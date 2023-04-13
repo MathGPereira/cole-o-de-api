9d1ef80e0cf53dfa1d7542273162e270167b4873
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-13 08:17:54 -0300

    Página da API de QR code finalizada

--- assets/js/api/qrcode.js
+++ assets/js/api/qrcode.js
@@ -1,16 +1,19 @@
-async function qrcode(informacao, tamanho="", secao) {
-    const url = `http://api.qrserver.com/v1/create-qr-code/?data=${informacao}&size=${tamanho}x${tamanho}`;
+export default async function qrcode(informacao, tamanho=200, charsetSource="UTF-8", charsetTarget="UTF-8", ecc="L", cor, secao) {
+    const url = `http://api.qrserver.com/v1/create-qr-code/?data=${informacao}&size=${tamanho}x${tamanho}&charset-source=${charsetSource}&charset-target=${charsetTarget}&bgcolor=${cor}&ecc=${ecc}`;
     const qr = await fetch(url);
 
     colocaQrCodeNaTela(qr, secao);
 }
 
 function colocaQrCodeNaTela(qr, secao) {
+    secao.innerHTML = "";
+
     const qrCodeGerado = `
         <img src="${qr.url}" alt="Código de barras do tipo QR Code" class="imagem-qrcode">
-        <a href="https://goqr.me/api/doc/create-qr-code/" target="_blank" rel="nofollow external" class="sobre-api__link-hd">Link para a documentação</a>
+        <a href="https://goqr.me/api/doc/create-qr-code/" target="_blank" rel="nofollow external" class="sobre-api__link-hd sobre-api__link-qr">Link para a documentação</a>
     `;
+
     secao.innerHTML += qrCodeGerado;
 }
 
-qrcode("Bem vindo a Galeria e API's", 200, document.querySelector("[data-sobre-api]"));
+qrcode("Bem vindo a Galeria e API's", 200, "UTF-8", "UTF-8", "L", "FFFFFF", document.querySelector("[data-sobre-api]"));
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import api from "./api/apod.js";
+import qrcode from "./api/qrcode.js";
 
 let count;
 let startDate;
@@ -8,16 +9,35 @@ const botaoRodaApi = document.querySelector("[data-roda-api]");
 const secao = document.querySelector("[data-resultado-api]");
 
 window.addEventListener("load", () => {
-    botaoRodaApi.addEventListener("click", evento => {
-        evento.preventDefault();
-        
-        count = document.querySelector("[data-count]").value;
-        startDate = document.querySelector("[data-start]").value;
-        endDate = document.querySelector("[data-end]").value;
-        date = document.querySelector("[data-date]").value;
+    try {
+        botaoRodaApi.addEventListener("click", evento => {
+            evento.preventDefault();
+            
+            count = document.querySelector("[data-count]").value;
+            startDate = document.querySelector("[data-start]").value;
+            endDate = document.querySelector("[data-end]").value;
+            date = document.querySelector("[data-date]").value;
+    
+            api(date, count, startDate, endDate, secao);
+        });    
 
-        api(date, count, startDate, endDate, secao);
-    });    
+        api(date, count, startDate, endDate, document.querySelector("[data-sobre-api]"));
+    }catch(erro) {
+        console.log(erro);
+    }
+});
 
-    api(date, count, startDate, endDate, document.querySelector("[data-sobre-api]"));
+const botaoRodaQrCode = document.querySelector("[data-roda-qrcode]");
+
+botaoRodaQrCode.addEventListener("click", evento => {
+    evento.preventDefault();
+
+    const conteudo = document.querySelector("[data-conteudo]").value;
+    const tamanho = document.querySelector("[data-tamanho]").value;
+    const source = document.querySelector("[data-source]").value;
+    const target = document.querySelector("[data-target]").value;
+    const ecc = document.querySelector("[data-ecc]").value;
+    const cor = document.querySelector("[data-cor]").value.replace("#", "");
+    
+    qrcode(conteudo, tamanho, source, target, ecc, cor, secao);
 });
